9a0cc30a9c9a2a79d827f30177ce20a1f2c301b5
khainam23 <khainam23@gmail.com>
2026-05-22 06:16:32 +0700

    258 Add Digits 263 Ugly Number

--- 258_Add_Digits.py
+++ 258_Add_Digits.py
@@ -36,3 +36,11 @@ class Solution:
 
         return sum(num)
             
+'''
+Super best solution
+class Solution(object): 
+    def addDigits(self, num): 
+        if num == 0: return 0
+        if num % 9 == 0: return 9
+        return num % 9
+'''
